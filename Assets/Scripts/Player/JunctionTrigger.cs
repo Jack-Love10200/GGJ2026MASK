5f6df10e1600e0226a6ed7e40c031096ab07ba5e
Jack Love <j.m.love1560@gmail.com>
2026-01-31 15:59:18 -0800

    Player controller

---
+++ Assets/Scripts/Player/JunctionTrigger.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+using UnityEditor.Experimental.GraphView;
+using System.Collections;
+
+public class JunctionTrigger : MonoBehaviour
+{
+
+  [Serializable]
+  public struct DirectionOption
+  {
+    public float angle;
+    public InputDirection inputDirection;
+  }
+
+  public Transform Destination;
+  public List<DirectionOption> OpenDirections;
+  public List<JunctionTrigger> Siblings;
+
+  public void Start()
+  {
+    
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+
+    Debug.Log("new trigger enter");
+
+    GetComponent<BoxCollider>().enabled = false;
+    foreach (JunctionTrigger tr in Siblings)
+    {
+      tr.GetComponent<BoxCollider>().enabled = false;
+    }
+
+    StartCoroutine(func());
+  }
+
+  IEnumerator func()
+  {
+    yield return new WaitForSeconds(10);
+
+    GetComponent<BoxCollider>().enabled = true;
+    foreach (JunctionTrigger tr in Siblings)
+    {
+      tr.GetComponent<BoxCollider>().enabled = true;
+    }
+  }
+
+  public void Update()
+  {
+    
+  }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,30 +1,119 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+
+[System.Serializable]
+public enum InputDirection
+{
+  Left,
+  Right,
+  Forward,
+}
 
 public class Player : MonoBehaviour
 {
+
+  enum MovementState
+  {
+    Moving,
+    Turning,
+  }
+
+  private struct TurnData
+  {
+    public float angle;
+
+    public Quaternion startRotation;
+    public Quaternion endRotation;
+
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+    public float timeInTurn;
+  }
+
+
+  [Header("Settings")]
+  public float Speed;
+  public float TurnTime;
+  private TurnData currentTurn;
+
+  private MovementState currentState = MovementState.Moving;
+
+
+  InputDirection desiredDirection = InputDirection.Forward;
+
   void Start()
   {
-    
+
   }
   void Update()
   {
-    transform.Translate(transform.forward, Space.World);
+    if (currentState == MovementState.Moving)
+    {
+      transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+    }
+    else if (currentState == MovementState.Turning)
+    {
+      currentTurn.timeInTurn += Time.deltaTime;
+
+      if (currentTurn.timeInTurn > TurnTime)
+        currentState = MovementState.Moving;
+
+      //lerp postion and rotation to the target
+      transform.position = Vector3.Lerp(currentTurn.startPosition, currentTurn.endPosition, currentTurn.timeInTurn / TurnTime);
+      transform.rotation = Quaternion.Slerp(currentTurn.startRotation, currentTurn.endRotation, currentTurn.timeInTurn / TurnTime);
+    }
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    currentState = MovementState.Turning;
+
+    Debug.Log("Trigger enter");
+
+    if (other.TryGetComponent<JunctionTrigger>(out JunctionTrigger junction))
+    {
+      List<JunctionTrigger.DirectionOption> options = junction.OpenDirections;
+
+      if (options.Count <= 0)
+        Debug.LogWarning("Junction has no open directions");
+
+      int optionIndex = options.FindIndex(option => option.inputDirection == desiredDirection);
+      if (optionIndex == -1) //player chose a direction they cant go, Jack gets to pick where they go now
+      {
+        optionIndex = Random.Range(0, options.Count - 1);
+      }
+
+      TurnData turn = new TurnData();
+      turn.angle = options[optionIndex].angle;
+      turn.startPosition = transform.position;
+      turn.endPosition = junction.Destination.position;
+      turn.startRotation = transform.rotation;
+      turn.endRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + turn.angle, 0);
+      turn.timeInTurn = 0f;
+
+      currentTurn = turn;
+    }
+
+    else
+      Debug.LogWarning("Player collided with not junction object");
+  }
+
+  public void OnMoveForward(InputAction.CallbackContext context)
+  {
+    if (context.started)
+      desiredDirection = InputDirection.Forward;
   }
 
   public void OnMoveLeft(InputAction.CallbackContext context)
   {
     if (context.started)
-    {
-      transform.Rotate(0, -90, 0);
-    }
+      desiredDirection = InputDirection.Left;
   }
 
   public void OnMoveRight(InputAction.CallbackContext context)
   {
     if (context.started)
-    {
-      transform.Rotate(0, 90, 0);
-    }
+      desiredDirection = InputDirection.Right;
   }
 }
