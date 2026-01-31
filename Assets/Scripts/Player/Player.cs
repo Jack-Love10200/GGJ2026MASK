15b0eeb9fd85c614b2cc1bbb9cc21449386b9315
Jack Love <j.m.love1560@gmail.com>
2026-01-31 00:54:53 -0800

    test level chunks for cam movement testing

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,14 +3,10 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-  // Start is called once before the first execution of Update after the MonoBehaviour is created;
-
   void Start()
   {
-
+    
   }
-
-  // Update is called once per frame
   void Update()
   {
     transform.Translate(transform.forward, Space.World);
