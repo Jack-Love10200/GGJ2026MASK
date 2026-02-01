1e2aacaa70583f8b9653206ac246bdee627c0026
GuardianEater <128861220+GuardianEater@users.noreply.github.com>
2026-01-31 23:57:57 -0800

    Added back shelf.

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -6,21 +6,21 @@ public class Shelf : MonoBehaviour
 {
     // public ////////////////////////////////////////////////
     public List<GameObject> propPrefabs;
-    public int propCount = 5;
+    public int PropCount = 5;
 
     // private ///////////////////////////////////////////////
-    private BoxCollider spawnArea;
-    private List<Bounds> placedBounds = new List<Bounds>();
+    private BoxCollider m_SpawnArea;
+    private List<Bounds> m_PlacedBounds = new List<Bounds>();
 
 
     void Start()
     {
-        spawnArea = GetComponent<BoxCollider>();
+        m_SpawnArea = GetComponent<BoxCollider>();
         int maxAttempts = 30;
-        float spawnAreaBottomY = spawnArea.center.y - spawnArea.size.y / 2f;
+        float spawnAreaBottomY = m_SpawnArea.center.y - m_SpawnArea.size.y / 2f;
         float worldSpawnAreaBottomY = transform.TransformPoint(new Vector3(0, spawnAreaBottomY, 0)).y;
 
-        for (int i = 0; i < propCount; i++)
+        for (int i = 0; i < PropCount; i++)
         {
             bool placed = false;
             int attempts = 0;
@@ -34,15 +34,15 @@ public class Shelf : MonoBehaviour
                 Renderer prefabRenderer = propPrefab.GetComponentInChildren<Renderer>();
                 Bounds propBounds = prefabRenderer.bounds;
 
-                float halfX = spawnArea.size.x / 2f;
-                float halfZ = spawnArea.size.z / 2f;
+                float halfX = m_SpawnArea.size.x / 2f;
+                float halfZ = m_SpawnArea.size.z / 2f;
 
                 Vector3 localPos = new Vector3(
                     Random.Range(-halfX, halfX),
                     spawnAreaBottomY,
                     Random.Range(-halfZ, halfZ)
                 );
-                localPos += spawnArea.center;
+                localPos += m_SpawnArea.center;
                 Vector3 worldPos = transform.TransformPoint(localPos);
                 float propBottomOffset = prefabRenderer.bounds.min.y - propPrefab.transform.position.y;
                 worldPos.y = worldSpawnAreaBottomY - propBottomOffset;
@@ -50,7 +50,7 @@ public class Shelf : MonoBehaviour
 
                 // check against all of the bounds we have placed
                 bool overlaps = false;
-                foreach (var p in placedBounds)
+                foreach (var p in m_PlacedBounds)
                 {
                     if (propBounds.Intersects(p))
                     {
@@ -67,7 +67,7 @@ public class Shelf : MonoBehaviour
 
                     // adds the bounds to all of the bounds we have placed
                     if (instanceRenderer != null)
-                        placedBounds.Add(instanceRenderer.bounds);
+                        m_PlacedBounds.Add(instanceRenderer.bounds);
                     placed = true;
                 }
             }
